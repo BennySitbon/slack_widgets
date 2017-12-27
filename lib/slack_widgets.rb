0971f42b23d9a03b6af450d651989bbf4a3fc675
Benny Sitbon <benny.sitbon@wework.com>
2017-12-27 18:43:32 +0200

    loading dialog class on startup

--- lib/slack_widgets.rb
+++ lib/slack_widgets.rb
@@ -1,6 +1,7 @@
 require "slack_widgets/version"
 require "slack_widgets/widgets/buttons/button.rb"
 require "slack_widgets/widgets/pickers/picker.rb"
+require "slack_widgets/widgets/dialogs/dialog.rb"
 
 module SlackWidgets
   Dir[File.dirname(__FILE__) + "/slack_widgets/**/*.rb"].each do |file|
--- lib/slack_widgets/version.rb
+++ lib/slack_widgets/version.rb
@@ -1,3 +1,3 @@
 module SlackWidgets
-  VERSION = "0.3.0"
+  VERSION = "0.3.1"
 end
